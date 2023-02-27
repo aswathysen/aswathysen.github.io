6e874ec3329f3f3efec7bb798443cbf34b3b617c
aswathysen <aswathy.sen@mnscorp.net>
2023-02-27 12:21:56 +0530

    edited config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'aswathysen', // Usually your GitHub org/user name.
-  projectName: 'github.io', // Usually your repo name.
+  projectName: 'aswathysen.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
